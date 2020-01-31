b2487d15d3787eebadd19cce48c528e938ca5426
Josenildolins <qa@devexo.com.br>
2020-01-31 14:15:55 -0300

    eav

---
+++ ACM/ACM.BL/CustomerRepository.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ACM.BL
+{
+    public class CustomerRepository
+    {
+        /// <summary>
+        /// Retrieve one customer.
+        /// </summary>
+        public Customer Retrieve(int customerId)
+        {
+            // Create the instance of the Customer class
+            // Pass in the requested id
+
+            Customer customer = new Customer(customerId);
+
+            if (customerId == 1)
+            {
+                customer.EmailAddress = "tasoha4484@eroyal.net";
+                customer.FirstName = "Frodo";
+                customer.Lastname = "Baggins";
+            }
+
+            return new Customer();
+        }
+
+        /// <summary>
+        /// Sves the current customer
+        /// </summary>
+        /// <returns></returns>
+        public bool Save()
+        {
+            // Code that saves the defined customer
+
+            return true;
+        }
+
+        /// <summary>
+        /// Retrieve all customers
+        /// </summary>
+        public List<Customer> Retrieve()
+        {
+            // Code that retrieves all of the customers
+
+            return new List<Customer>();
+        }
+
+    }
+}
---
+++ ACM/Tests/ACM.BL.Test/CustomerRepositoryTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ACM.BL.Test
+{
+    [TestClass]
+    public class CustomerRepositoryTest
+    {
+        [TestMethod]
+        public void RetrieveValid()
+        {
+            // -- Arrange
+
+            var customerRepository = new CustomerRepository();
+            var expected = new Customer(1)
+            {
+                EmailAddress = "tasoha4484@eroyal.net",
+                FirstName = "Frodo",
+                Lastname = "Baggings"
+            };
+
+            //-- Act
+
+            var actual = customerRepository.Retrieve(1);
+
+            //-- Assert
+
+            Assert.AreEqual(expected.CustomerId,actual.CustomerId);
+            Assert.AreEqual(expected.EmailAddress,actual.EmailAddress);
+            Assert.AreEqual(expected.FirstName,actual.FirstName);
+            Assert.AreEqual(expected.Lastname,actual.Lastname);
+        }
+    }
+}
