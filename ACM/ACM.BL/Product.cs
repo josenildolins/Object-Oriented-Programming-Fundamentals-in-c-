686c3fc0460c09284b5813d63d692bb1da30083a
Josenildolins <qa@devexo.com.br>
2020-02-06 13:55:34 -0300

    Definindo a base class

--- ACM/ACM.BL/Product.cs
+++ ACM/ACM.BL/Product.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace ACM.BL
 {
-    class Product
+    class Product : EntityBase
     {
         public Product()
         {
@@ -23,11 +23,14 @@ namespace ACM.BL
         public int ProductId { get; private set; }
         public string ProductName { get; set; }
 
+
+        public override string ToString() => ProductName;
+
         /// <summary>
-        /// Validate the customer data.
+        /// Validate the Product data.
         /// </summary>
         /// <returns></returns>
-        public bool Validate()
+        public override bool Validate()
         {
             var isValid = true;
 
